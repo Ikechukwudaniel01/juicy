f20ac20b5b36979473ff0f0ee6094dd7142d280b
Ikechukwudaniel01 <dikechukwu402@gmail.com>
2024-11-05 11:00:28 -1100

    Add files via upload

---
+++ SEN ASSIGNMENT.py
@@ -0,0 +1,8 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print( Hello World )
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print'(hello world)
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print("Hello world")
+Hello world
